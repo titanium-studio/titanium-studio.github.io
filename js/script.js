26f413d5167fcf45023461b4599fc9d8031edf52
NoNameTitan <50516342+NoNameTitan@users.noreply.github.com>
2019-10-20 00:13:42 +0500

    please

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 var foo = $id('angle')
 var fyy = $id('float')
+var card_in = $id("card_in")
+var card_move = $id("card_move")
 
 foo.addEventListener("mouseover",enter)
 foo.addEventListener("mouseleave",out)
@@ -18,4 +20,10 @@ foo.onclick=()=>{
   console.log(window);
   let a = window.innerHeight
   window.scrollBy(0,a)
+}
+card_in.onclick=()=>{
+  card_move.classList.add('card_move')
+  card_move.classList.remove('delete')
+  card_in.classList.remove('in')
+  card_in.classList.remove('color_2')
 }
