46caf6c0754a33eb77efe3f6ac7afccf272f44eb
NoNameTitan <50516342+NoNameTitan@users.noreply.github.com>
2020-08-29 15:11:32 +0500

    end

--- js/index.js
+++ js/index.js
@@ -63,6 +63,7 @@ const __sections__ = $all("section")
 const __content__ = $all(".content")
 const __titles__ = $all(".title")
 const ELEMENTS = $all(".slide")
+const BUTTONS = $all(".btn")
 
 // For Function param[s]
 const idlePeriod = 100
@@ -111,7 +112,7 @@ function nav_on() {
         for (let i = 0; i < __titles__.length; i++) {
             const title = __titles__[i]
             add(title, "fly")
-            add(__content__[i], "none")
+            add(__sections__[i], "none")
             title.style.top = `${(i * 12) + 30}vh`
             title.style.fontSize = "10vh"
         }
@@ -124,9 +125,11 @@ function nav_off() {
         for (let i = 0; i < __titles__.length; i++) {
             const title = __titles__[i]
             remove(title, "fly")
-            remove(__content__[i], "none")
+            remove(__sections__[i], "none")
             title.style.top = ``
             title.style.fontSize = ""
+            $("#circle").style.width = ""
+            $("#circle").style.height = ""
         }
     }
 }
@@ -138,9 +141,7 @@ function setup(ev) {
 function wheel_target(event) {
     var delta = event.wheelDelta
     var timeNow = new Date().getTime()
-    // Cancel scroll if currently animating or within quiet period
     if (timeNow - lastAnimation < idlePeriod + animationDuration) {
-        // event.preventDefault()
         return;
     }
 
@@ -158,19 +159,34 @@ function title_function(target) {
     target.addEventListener("click", function () {
         let link = $id(this.getAttribute("link"))
         nav_off()
-        __sections__.forEach((section, i) => {
+        __sections__.forEach((section) => {
             remove(section, "active")
-            add(link, "active")
         })
+        add(link, "active")
         smooth(link)
+    })
+    target.addEventListener("mouseenter", () => {
+        if (contains(target, "fly")) {
+            $("#circle").style.width = "var(--size5)"
+            $("#circle").style.height = "var(--size5)"
+        }
+    })
+    target.addEventListener("mouseleave", () => {
+        if (contains(target, "fly")) {
+            $("#circle").style.width = ""
+            $("#circle").style.height = ""
+        }
     })
 }
 
 // Add Event Listener[s]
-window.addEventListener("resize", () => { smooth($(".section.active")) })
+window.addEventListener("resize", () => {
+    smooth($(".section.active"))
+    reDraw()
+})
 document.readyState == 'loading' ? document.addEventListener("DOMContentLoaded", setup) : setTimeout(setup, 10)
 document.addEventListener("wheel", wheel_target)
-__hero__.addEventListener("click", ()=>{ smooth($id("DSGN"))})
+__hero__.addEventListener("click", () => { smooth($id("DSGN")) })
 __nav__.addEventListener("click", nav_view)
 __plane__.addEventListener("click", plane_function)
 __titles__.forEach(title_function)
@@ -189,7 +205,135 @@ ELEMENTS.forEach((target, index) => {
         ELEMENTS_SPAN[index].style.left = e.pageX - target.offsetLeft + "px"
         ELEMENTS_SPAN[index].style.top = e.pageY - target.offsetTop + "px"
     })
+});
+(() => {
+    var xmouse, ymouse;
+    document.addEventListener('mousemove', (e) => {
+        xmouse = e.clientX || e.pageX;
+        ymouse = e.clientY || e.pageY;
+    });
+
+    var circle = $('#circle')
+    var zx = void 0,
+        zy = void 0,
+        zdx = void 0,
+        zdy = void 0;
+
+    var ball = $('#ball')
+    var x = void 0,
+        y = void 0,
+        dx = void 0,
+        dy = void 0;
+
+    var followMouse = function followMouse() {
+        requestAnimationFrame(followMouse)
+
+        if (!zx || !zy) {
+            zx = xmouse
+            zy = ymouse
+        } else {
+            zdx = (xmouse - zx) * 0.5
+            zdy = (ymouse - zy) * 0.5
+            if (Math.abs(zdx) + Math.abs(zdy) < 0.1) {
+                zx = xmouse
+                zy = ymouse
+            } else {
+                zx += zdx
+                zy += zdy
+            }
+        }
+
+        if (!x || !y) {
+            x = xmouse;
+            y = ymouse;
+        } else {
+            dx = (xmouse - x) * 0.125;
+            dy = (ymouse - y) * 0.125;
+            if (Math.abs(dx) + Math.abs(dy) < 0.1) {
+                x = xmouse
+                y = ymouse
+            } else {
+                x += dx
+                y += dy
+            }
+        }
+        ball.style.left = x + 'px'
+        ball.style.top = y + 'px'
+
+        circle.style.left = zx + 'px'
+        circle.style.top = zy + 'px'
+
+    };
+    followMouse()
+})();
+BUTTONS.forEach((target) => {
+    target.addEventListener("mouseenter", () => {
+        $("#circle").style.width = "var(--size5)"
+        $("#circle").style.height = "var(--size5)"
+    })
+    target.addEventListener("mouseleave", () => {
+        $("#circle").style.width = ""
+        $("#circle").style.height = ""
+    })
+});
+
+
+let circles = $all(".circle")
+let opts = {
+    size: window.innerWidth / 6,
+    radius: window.innerWidth / 18,
+    lineWidth: window.innerWidth / 18 / 6,
+    /**@type {HTMLCanvasElement[]} */
+    listCanvas: [],
+    listSpan: []
+}
+/**
+ * @param {CanvasRenderingContext2D} ctx
+ * @param {String} color
+ * @param {Number} percent_
+ */
+var drawCircle = function (ctx, color, percent_) {
+
+    percent = Math.min(Math.max(0, percent_ || 1), 1)
+    ctx.beginPath()
+    ctx.arc(0, 0, opts.radius, 0, Math.PI * 2 * (percent_ / 100), false)
+    ctx.strokeStyle = color
+    ctx.lineCap = 'round'
+    ctx.lineWidth = opts.lineWidth
+    ctx.stroke()
+    ctx.closePath()
+}
+
+circles.forEach((circle, i) => {
+    let { listCanvas, listSpan } = opts
+    let percent = circle.getAttribute("data-percent") || 0
+
+    listCanvas[i] = document.createElement('canvas')
+    listSpan[i] = document.createElement("span")
+    listSpan[i].textContent = percent + "%"
+
+    circle.appendChild(listCanvas[i])
+    circle.appendChild(listSpan[i])
 })
+reDraw = () => {
+    opts.radius = window.innerWidth / 6 / 3
+    opts.size = window.innerWidth / 6
+    opts.lineWidth = Math.max(window.innerWidth / 6 / 3 / 6, 5)
+    circles.forEach((circle, i) => {
+        let { listCanvas, listSpan } = opts
+        let percent = circle.getAttribute("data-percent") || 0
+
+        listCanvas[i].width = listCanvas[i].height = opts.size
+        let ctx = listCanvas[i].getContext("2d")
+        ctx.translate(opts.size / 2, opts.size / 2)
+        ctx.rotate(Math.PI * (- 0.5))
+
+        ctx.clearRect(0, 0, opts.size, opts.size)
+        drawCircle(ctx, "#efefef", 100)
+        drawCircle(ctx, "#555555", percent)
+    })
+}
+reDraw()
 
 /** For Experiment[s] */
 let __target = document.createElement("div")
@@ -197,79 +341,3 @@ let __target = document.createElement("div")
 
 
 
-// ;(()=>{
-//     var xmouse, ymouse;
-//     document.addEventListener('mousemove', (e) => {
-//         xmouse = e.clientX || e.pageX;
-//         ymouse = e.clientY || e.pageY;
-//     });
-
-//     var circle = $('#circle')
-//     var zx = void 0,
-//         zy = void 0,
-//         zdx = void 0,
-//         zdy = void 0;
-
-//     var matrix = {
-//         x: 1,
-//         y: 1
-//     }
-//     var ball = $('#ball')
-//     var x = void 0,
-//         y = void 0,
-//         dx = void 0,
-//         dy = void 0,
-//         key = -1;
-
-//     var followMouse = function followMouse() {
-//         key = requestAnimationFrame(followMouse)
-
-
-
-//         if (!zx || !zy) {
-//             zx = xmouse
-//             zy = ymouse
-//         } else {
-//             zdx = (xmouse - zx) * 0.5
-//             zdy = (ymouse - zy) * 0.5
-//             if (Math.abs(zdx) + Math.abs(zdy) < 0.1) {
-//                 zx = xmouse
-//                 zy = ymouse
-//             } else {
-//                 zx += zdx
-//                 zy += zdy
-//             }
-//         }
-
-//         if (!x || !y) {
-//             x = xmouse;
-//             y = ymouse;
-//         } else {
-//             dx = (xmouse - x) * 0.125;
-//             dy = (ymouse - y) * 0.125;
-//             if (Math.abs(dx) + Math.abs(dy) < 0.1) {
-//                 x = xmouse
-//                 y = ymouse
-//             } else {
-//                 x += dx
-//                 y += dy
-//             }
-//         }
-//         ball.style.left = x + 'px'
-//         ball.style.top = y + 'px'
-
-//         circle.style.left = zx + 'px'
-//         circle.style.top = zy + 'px'
-//         if (zx != x) {
-//             matrix.x = 1.8
-//         } else {
-//             matrix.x = 1
-//         }
-//         if (zy != y) {
-//             matrix.y = 1.8
-//         } else {
-//             matrix.y = 1
-//         }
-        
-//     };
-// })()
