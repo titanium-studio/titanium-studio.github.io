6fb9e77dffa8f8b2b75f0157a086c6e501a8b927
NoNameTitan <50516342+NoNameTitan@users.noreply.github.com>
2020-12-27 19:16:23 +0500

    e

--- js/index.js
+++ js/index.js
@@ -60,6 +60,7 @@ const __hero__ = $id("hero")
 const __body__ = $id("body")
 const __front__ = $id("front")
 const __back__ = $id("back")
+const __footer__ = $(".footer")
 const __nav__ = $id("nav") // Nav Button
 const __plane__ = $id("plane")
 const __sections__ = $all("section")
@@ -114,6 +115,7 @@ function nav_on() {
     if (!contains(__nav__, "active")) {
         add(__nav__, "active")
         add(__back__, "active")
+        add(__footer__, "active")
         for (let i = 0; i < __titles__.length; i++) {
             const title = __titles__[i]
             add(title, "fly")
@@ -127,6 +129,7 @@ function nav_off() {
     if (contains(__nav__, "active")) {
         remove(__nav__, "active")
         remove(__back__, "active")
+        remove(__footer__, "active")
         for (let i = 0; i < __titles__.length; i++) {
             const title = __titles__[i]
             remove(title, "fly")
@@ -168,6 +171,10 @@ function plane_function(ev) {
     add($id("CALL"), "active")
     smooth($id("CALL"))
 }
+function reSize() {
+    let a = contains($("body"),"dark_mode")
+    a ? reDraw("#333","#244dff") : reDraw()
+}
 function title_function(target) {
     target.addEventListener("click", function () {
         let link = $id(this.getAttribute("link"))
@@ -195,14 +202,20 @@ function title_function(target) {
 //#region Add Event Listener[s]
 window.addEventListener("resize", () => {
     smooth($(".section.active"))
-    reDraw()
+    reSize()
 })
 $$.readyState == 'loading' ? $$.addEventListener("DOMContentLoaded", setup) : setTimeout(setup, 10)
-$$.addEventListener("wheel", wheel_target,{passive:false})
+$$.addEventListener("wheel", wheel_target, { passive: false })
 __hero__.addEventListener("click", () => { smooth($id("DSGN")) })
 __nav__.addEventListener("click", nav_view)
 __plane__.addEventListener("click", plane_function)
 __titles__.forEach(title_function)
+$(".dark_mode_change").addEventListener("click", () => {
+    toggle($("body"), "dark_mode")
+    reSize()
+})
+//#endregion
+//#region Hover Event
 ELEMENTS.forEach((target, index) => {
     if (!ELEMENTS_SPAN[index])
         ELEMENTS_SPAN[index] = target.querySelector("span")
@@ -291,7 +304,7 @@ BUTTONS.forEach((target) => {
     })
 });
 //#endregion
-
+//#region Progress circle draw with canvas 
 let circles = $all(".circle")
 let opts = {
     size: window.innerWidth / 6,
@@ -329,7 +342,8 @@ circles.forEach((circle, i) => {
     circle.appendChild(listCanvas[i])
     circle.appendChild(listSpan[i])
 })
-reDraw = () => {
+reDraw = (firstColor = "#efefef",secondColor = "#555555") => {
+    secondColor ="#244dff"
     opts.radius = window.innerWidth / 6 / 3
     opts.size = window.innerWidth / 6
     opts.lineWidth = Math.max(window.innerWidth / 6 / 3 / 6, 5)
@@ -343,12 +357,12 @@ reDraw = () => {
         ctx.rotate(Math.PI * (- 0.5))
 
         ctx.clearRect(0, 0, opts.size, opts.size)
-        drawCircle(ctx, "#efefef", 100)
-        drawCircle(ctx, "#555555", percent)
+        drawCircle(ctx, firstColor, 100)
+        drawCircle(ctx, secondColor, percent)
     })
 }
 reDraw()
-
+//#endregion
 //#region Experiment[s] Zone
 /**For Experiments element */
 let __target = $$.createElement("div")
@@ -357,7 +371,7 @@ var isMobile, isMobileAndTablet;
 const mobileCheck = function () {
     let check = false;
     (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-    
+
     isMobile = check
     return check;
 };
@@ -384,7 +398,7 @@ setTimeout(() => {
     $$.getElementById("mouse").classList.remove("loader")
     $$.getElementById("ball").classList.add("invert")
     $$.getElementById("ball").innerHTML = ""
-    if (isMobile){
+    if (isMobile) {
         $id("mouse").style.display = "none"
         $id("ball").style.display = "none"
     }
