827acf879ccd6f693627ec4c76cb6151d42e74f8
Aset Telmanov <50516342+NoNameTitan@users.noreply.github.com>
2021-03-26 22:24:20 +0500

    .

--- gallery/js/imageiterate.js
+++ gallery/js/imageiterate.js
@@ -1,14 +1,26 @@
+let imgs = [{ x: new Image, y: [""] }]
+
 getData("/src/json/gallery.json", (r, d) => {
   if (r) throw new Error(r)
-  let a =d.gallery, b = a.data, с = window.location.origin + a.corePath, z = $id("gallery");
-  forEach(b, x => z.appendChild(imageItem(с + x.src, x.tags)))
+  let a = d.gallery, b = a.data, с = window.location.origin + a.corePath, z = $id("gallery");
+  forEach(b, x => z.appendChild(imageItem(с, x.src, x.tags)))
 })
 
-function imageItem(img, alt) {
+function filter(query) {
+  let r = []
+  imgs.forEach(x => x.y.includes(query) ? r.push(x.x) : void 0)
+  return r
+}
+
+function imageItem(path, img, alt) {
   let x = $$.createElement("div"), y = new Image();
+  imgs.push({
+    x: x,
+    y: alt
+  })
   x.setAttributeNode($$.createAttribute("item"))
   add(x, ["flex", "to_center"])
-  y.src = img
+  y.src = path + img
   y.setAttribute("width", "100%")
   if (is.array(alt)) y.setAttribute("alt", alt.join(", "))
   y.addEventListener("load", () => (x.appendChild(y)), { once: true })
