d0d71e684603942f2b823a444d022d94784516c4
Aset Telmanov <50516342+NoNameTitan@users.noreply.github.com>
2021-04-09 17:45:55 +0500

    .

--- js/index.js
+++ js/index.js
@@ -8,9 +8,34 @@ const { $id, add, remove, contains } = html,
   { _nav_, _back_, _body_ } = gVars,
   $$ = $id("body"),
   main = Main.init();
+let error = false
+//#region LoadData
+getJSON("/src/json/index.json", (err, res) => {
+  if (error = err) return _body_.innerHTML = "<h1 class='center_float half'>Unfortunately this page doesn't work</h1>"
 
+  let core = window.location.origin + res.index.corePath, imgs = res.index.img,
+    set = (i, ...v) => { return main.__sections__[i].setContent(...v) },
+    b_h = box_height;
+
+  // DSGN
+  forEach(imgs, (x, i) => set(0, b_h(box(true, core + x.url, x.p), { gridArea: "x" + i })).reverse(true))
+
+  // WORK
+  forEach(res.work.data, x => set(1, b_h(card().addH2(x.name).addP(x.description).addMore(x.more).self)))
+
+  // SKILLS
+  forEach(res.skills.data, x => set(2, b_h([Div.x("name").setHTML(x.name).self, Div.x("icon").setHTML(x.icon).self])))
+
+  // CALL
+  forEach(res.social.data, x => forIn(x, y => set(3, b_h(btn(link(y, x[y]), true)))))
+})
+//#endregion
+
+//#region LazyLoad
 main.timeOut(() => {
+  if(error) return
   main.view($$).event(_nav_, "click", () => contains(_nav_, "active") ? main.nav_off() : main.nav_on())
+  remove(_back_, "active")
 
   if (isMobile) main.changeFavicon("phone.png")
   else {
@@ -20,31 +45,6 @@ main.timeOut(() => {
     add(mouse.mouse, "invert")
     add(mouse.ball, "invert")
   }
-
-  getJSON("/src/json/index.json", (err, res) => {
-    if (err) return _body_.innerHTML = "<h1 class='center_float half'>Unfortunately this page doesn't work</h1>"
-    remove(_back_, "hide")
-
-    let core = window.location.origin + res.index.corePath, imgs = res.index.img,
-      set = (i, ...v) => { return main.__sections__[i].setContent(...v) },
-      b_h = box_height;
-
-    // DSGN
-    forEach(imgs, (x, i) => set(0, b_h(box(true, core + x.url, x.p), { gridArea: "x" + i })).reverse(true))
-
-    // WORK
-    forEach(res.work.data, x => set(1, b_h(card().addH2(x.name).addP(x.description).addMore(x.more).self)))
-
-    // SKILLS
-    forEach(res.skills.data, x => {
-      let y = Div("icon"), z = Div("name");
-      y.innerHTML = x.icon
-      z.innerHTML = x.name
-      set(2, b_h([z, y]))
-    })
-
-    // CALL
-    forEach(res.social.data, x => forIn(x, y => set(3, b_h(btn(link(y, x[y]), true)))))
-  })
 }, 1500)
+//#endregion
 export default main
--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
 import { html, tools, Box, gVars } from "./tools.js"
 import mouse from "./mouse.js"
-const { $, $id, $event, smooth, contains, add, remove } = html
-const { is, forEach } = tools
-const { section } = Box
-const { _nav_, _back_, _plane_, _logo_, _front_ } = gVars
+const { $, $id, $event, smooth, contains, add, remove } = html,
+  { forEach, EXTEND } = tools,
+  { section } = Box,
+  { _nav_, _back_, _plane_, _logo_, _front_ } = gVars;
 
 class Main {
   constructor() {
     this.__sections__ = []
-    this.HeaderElements = {
-      __hero__: [],
-      _nav_: [],
-      _plane_: [],
-    }
-    this.Background = []
-    this.WorkSection_buttons = []
-    this.values = {}
-    this.percentCircles = []
-    Main.Self = this
+    EXTEND(this, proto)
   }
 
   //#region Static
@@ -42,24 +33,22 @@ class Main {
     main.firstPage()
     return main
   }
-  static is = is
-  /** @type { Main } */
-  static #self
-  static set Self(value) { if (Main.is.empty(Main.#self) && value instanceof Main) Main.#self = value }
-  static get Self() { return Main.#self }
   //#endregion
 
-  //#region Public Functions
+}
+
+//#region Public Functions
+const proto = {
   view(x) {
     remove(_front_, "hide")
     x.innerHTML = ""
     forEach(this.__sections__, s => x.appendChild(s.self))
     return this
-  }
+  },
   reSize() {
     smooth($("section.active"))
-    return Main.Self
-  }
+    return this
+  },
   /**
    * @param {HTMLElement} el
    */
@@ -67,7 +56,7 @@ class Main {
     let d = "mouse", m = mouse[d], a = "style", b = "width", c = "height";
     $event(x, d + "enter", () => m[a][b] = m[a][c] = "var(--size5)")
     $event(x, d + "leave", () => m[a][b] = m[a][c] = "")
-  }
+  },
   /**
    * @param { string } eventName
    * @param { () => void } callback
@@ -75,86 +64,82 @@ class Main {
   event(x, eventName, callback, options = {}) {
     $event(x, eventName, callback, options)
     return this
-  }
+  },
   firstPage() {
-    let s = Main.Self.__sections__
+    let s = this.__sections__
     s.forEach(x => remove(x.self, "active"))
     add(s[0].self, "active")
     smooth(s[0].self)
     return this
-  }
+  },
   addScroll() {
-    let idlePeriod = 100,
-      animationDuration = 500,
-      lastAnimation = 0;
+    let t = 100, aT = 500, lT = 0;
 
     this.event(window, "wheel", e => {
       e.preventDefault()
-      let delta = e.wheelDelta, timeNow = new Date().getTime();
-      if (timeNow - lastAnimation < idlePeriod + animationDuration) return
-      delta < 0 ? this.nextPage() : this.prevPage()
-      lastAnimation = timeNow
+      let tn = new Date().getTime();
+      if (tn - lT < t + aT) return
+      e.wheelDelta < 0 ? this.nextPage() : this.prevPage()
+      lT = tn
     }, { passive: false })
     return this
-  }
+  },
   changeFavicon(url) {
     $id("favicon").attributes.getNamedItem("href").value = "./src/png/" + url
     return this
-  }
-  #x(n) { let a = Main.Self.__sections__[n].self; add(a, "active"); smooth(a) }
+  },
+  _(n) { let a = this.__sections__[n].self; add(a, "active"); smooth(a) },
   nextPage() {
-    let a = this.#x, s = this.__sections__
+    let _ = this._, s = this.__sections__
     forEach(s, (x, i) => {
       if (contains(x.self, "active")) {
         remove(x.self, "active")
-        if (i == s.length - 1) a(0)
-        else a(i + 1)
+        if (i == s.length - 1) _(0)
+        else _(i + 1)
         return true
       }
     })
-  }
+  },
   prevPage() {
-    let a = this.#x, s = this.__sections__
+    let _ = this._, s = this.__sections__
     forEach(this.__sections__, (x, i) => {
       if (contains(x.self, "active")) {
         remove(x.self, "active")
-        if (i == 0) a(s.length - 1)
-        else a(i - 1)
+        if (i == 0) _(s.length - 1)
+        else _(i - 1)
         return true
       }
     })
-  }
+  },
   nav_on() {
     add(_nav_, "active")
     add(_back_, "active")
     forEach(this.__sections__, x => add(x.self, ["fly", "none", "small"]))
     return this
-  }
+  },
   nav_off() {
     remove(_nav_, "active")
     remove(_back_, "active")
     forEach(this.__sections__, x => remove(x.self, ["fly", "none", "small"]))
     return this
-  }
+  },
   /**
-   * @param {( main: Main ) => void } callback
+   * @param {( main: Main ) => void } fn
    * @param { number } time
    */
-  timeOut(callback, time) {
-    let s = Main.Self
-    setTimeout(callback, time, s)
+  timeOut(fn, time) {
+    setTimeout(fn, time, this)
     return this
-  }
+  },
   /**
-   * @param {( main: Main ) => void } callback -
+   * @param {( main: Main ) => void } fn
    */
-  endLoading(callback, recursion = 0) {
-    let s = Main.Self
-    if ($$.readyState !== "loading" && recursion > 0) callback(s)
-    else s.timeOut(() => s.endLoading(callback, ++recursion), 10)
+  endLoading(fn, recursion = 0) {
+    if ($$.readyState !== "loading" && recursion > 0) fn(this)
+    else this.timeOut(() => this.endLoading(fn, ++recursion), 100)
     return this
   }
-  //#endregion
 }
+//#endregion
 
 export default Main
