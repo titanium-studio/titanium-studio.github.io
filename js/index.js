33641d6988d1fc7e2f9fc9277f050c5c867b2904
NoNameTitan <50516342+NoNameTitan@users.noreply.github.com>
2021-05-06 22:00:49 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,17 +4,18 @@ import mouse from "./mouse.js"
 
 const { $id, add, remove, contains } = html,
   { box_height, box, card, btn, link, Div } = Box,
-  { isMobile, forEach, forIn } = tools,
+  { isMobile, forEach, forIn, is } = tools,
   { _nav_, _back_, _body_ } = gVars,
+  data = localStorage,
   $$ = $id("body"),
   main = Main.init();
 let error = false
 
 //#region LoadData
-getJSON("/src/json/index.json", (err, res) => {
-  if (error = err) return _body_.innerHTML = "<h1 class='center_float half'>Unfortunately this page doesn't work</h1>"
+getJSON("/src/json/index.json", (r, z) => {
+  if (error = r) return _body_.innerHTML = "<h1 class='center_float half'>Unfortunately this page doesn't work</h1>"
 
-  let core = window.location.origin + res.index.corePath, imgs = res.index.img,
+  let core = window.location.origin + z.index.corePath, imgs = z.index.img,
     set = (i, ...v) => { return main.__sections__[i].setContent(...v) },
     b_h = box_height;
 
@@ -22,13 +23,13 @@ getJSON("/src/json/index.json", (err, res) => {
   forEach(imgs, (x, i) => set(0, b_h(box(true, core + x.url, x.p), { gridArea: "x" + i })).reverse(true))
 
   // WORK
-  forEach(res.work.data, x => set(1, b_h(card().addH2(x.name).addP(x.description).addMore(x.more).self)))
+  forEach(z.work.data, x => set(1, b_h(card().addH2(x.name).addP(x.description).addMore(x.more).self)))
 
   // SKILLS
-  forEach(res.skills.data, x => set(2, b_h([Div.x("name").setHTML(x.name).self, Div.x("icon").setHTML(x.icon).self])))
+  forEach(z.skills.data, x => set(2, b_h([Div.x("name").setHTML(x.name).self, Div.x("icon").setHTML(x.icon).self])))
 
   // CALL
-  forEach(res.social.data, x => forIn(x, y => set(3, b_h(btn(link(y, x[y]), true)))))
+  forEach(z.social.data, x => forIn(x, y => set(3, b_h(btn(link(y, x[y]), true)))))
 })
 //#endregion
 
