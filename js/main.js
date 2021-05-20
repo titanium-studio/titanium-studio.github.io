d4c3451d995ec98d217ca5053fb83681a72ca97c
Aset Telmanov <50516342+titanuim-x@users.noreply.github.com>
2021-05-20 06:50:10 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { tools, DOM } from "https://noname-titan.github.io/Tools/index.js"
+import { tools, DOM } from "https://titanuim-studio.github.io/Tools/index.js"
 import { Box, gVars } from "./tools.js"
 import mouse from "./mouse.js"
 
