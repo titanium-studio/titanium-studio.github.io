3c531aeee1766ff0432f70828d6a960a9e6d83b0
NoNameTitan <50516342+x-titan@users.noreply.github.com>
2021-05-23 21:04:13 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,54 +3,42 @@ import Main from "./main.js"
 import { Box, gVars } from "./tools.js"
 import mouse from "./mouse.js"
 
-const { each, getData } = tools,
+const { each } = tools,
   { search, add, remove, contains, styler, device } = DOM,
   { box_height, box, card, btn, link, Div, $event } = Box,
   { _nav_, _back_, _body_ } = gVars,
   $$ = search.id("body"),
   main = Main.init();
 
-let data = false, error = false;
-
 //#region LoadData
-getData("/src/json/index.json", (r, z) => {
-  if (error = r) return main.error()
+fetch("/src/json/index.json")
+  .then(x => x.json())
+  .then(z => {
+    let core = window.location.origin + z.index.corePath, imgs = z.index.img,
+      set = (i, ...v) => main.__sections__[i].setContent(...v),
+      b_h = box_height;
 
-  let core = window.location.origin + z.index.corePath, imgs = z.index.img,
-    set = (i, ...v) => { return main.__sections__[i].setContent(...v) },
-    b_h = box_height;
+    // DSGN
+    each(imgs, (x, i) => set(0, b_h(box(true, core + x.url, x.p), { gridArea: "x" + i })).reverse(true))
 
-  // DSGN
-  each(imgs, (x, i) => set(0, b_h(box(true, core + x.url, x.p), { gridArea: "x" + i })).reverse(true))
+    // WORK
+    each(z.work.data, x => set(1, b_h(card().addH2(x.name).addP(x.description).addMore(x.more).self)))
 
-  // WORK
-  each(z.work.data, x => set(1, b_h(card().addH2(x.name).addP(x.description).addMore(x.more).self)))
+    // SKILLS
+    each(z.skills.data, x => set(2, b_h([Div.x("name").setHTML(x.name).self, Div.x("icon").setHTML(x.icon).self])))
 
-  // SKILLS
-  each(z.skills.data, x => set(2, b_h([Div.x("name").setHTML(x.name).self, Div.x("icon").setHTML(x.icon).self])))
-
-  // CALL
-  each(z.social.data, x => each.obj(x, (y, z) => set(3, b_h(btn(link(z, y, "_blank"), true)))))
-  data = !data
-})
-//#endregion
-
-//#region LazyLoad
-function init() {
-  if (error) return main.error()
-  if (!data) return setTimeout(init, 200)
-  main.view($$)
-  $event(_nav_, "click", () => contains(_nav_, "active") ? main.nav_off() : main.nav_on())
-  remove(_back_, "active")
-  if (!device.isMobile) {
-    main.addScroll()
-    mouse.start()
-    mouse.view()
-    add(mouse.mouse, "invert")
-    add(mouse.ball, "invert")
-  }
-}
-document.addEventListener("DOMContentLoaded", init, { once: true })
+    // CALL
+    each(z.social.data, x => each.obj(x, (y, z) => set(3, b_h(btn(link(z, y, "_blank"), true)))))
+  })
+  .then(() => {
+    main.view($$); remove(_back_, "active")
+    $event(_nav_, "click", () => contains(_nav_, "active") ? main.nav_off() : main.nav_on())
+    if (!device.isMobile) {
+      main.addScroll(); mouse.start(); mouse.view()
+      add(mouse.mouse, "invert"); add(mouse.ball, "invert")
+    }
+  })
+  .catch(err => main.error(console.error(err)))
 //#endregion
 
 export default main
