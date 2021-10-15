5c8f1810363d1db7eed3e59276e6b2eb5ae72088
Aset Telmanov <telmanov2002.at@gmail.com>
2021-10-15 12:12:22 +0500

    fix image

--- gallery/js/imageiterate.js
+++ gallery/js/imageiterate.js
@@ -28,25 +28,6 @@ fetch("/src/json/gallery.json")
     }
     dd.onclick(); dd.onclick()
   })
-// getData("/src/json/gallery.json", (r, d) => {
-//   if (r) throw new Error(r)
-//   let a = d.gallery, b = a.data, с = window.location.origin + a.corePath, z = search.id("gallery"),
-//     aa = [], bb = [], cc = 0, dd = search.id("loadMore");
-//   each(b, (x, i) => {
-//     if (i % 5 == 0 && i !== 0) { aa.push(bb); bb = [] }
-//     bb.push({ c: с, s: x.src, f: a.format, t: x.tags })
-//   })
-//   if (!aa.includes(bb)) aa.push(bb)
-//   dd.onclick = () => {
-//     src.value = ""; filter("")
-//     if (cc < aa.length) {
-//       each(aa[cc], x => z.appendChild(imageItem(x.c, x.s, x.f, x.t)))
-//       if (aa.length - 1 == cc) add(dd, h)
-//       cc++
-//     } else add(dd, h)
-//   }
-//   dd.onclick(); dd.onclick()
-// })
 
 src.addEventListener("input", () => filter(src.value))
 
@@ -60,10 +41,10 @@ function addDataList(opt) {
 }
 
 function imageItem(path, img, format, alt = []) {
-  let x = $$.createElement("div"), y = new Image(); imgs.push({ x: x, y: alt.map(x => x.toLowerCase()) })
-  each(alt, a => addDataList(a)); styler(x, { width: "100%", aspectRatio: "1/1", overflow: "hidden" })
+  let x = $$.createElement("div"), y = new Image();  imgs.push({ x: x, y: alt.map(x => x.toLowerCase()) })
+  each(alt, a => addDataList(a));  styler(x, { width: "100%", aspectRatio: "1/1", overflow: "hidden" })
   add(x, "flex", "to_center"); y.src = path + img + "." + format; y.setAttribute("alt", alt.join(", "))
-  y.addEventListener("load", () => { y.style[y.width > y.height ? "height" : "width"] = "100%"; x.appendChild(y) }, { once: true })
+  y.addEventListener("load", () => { y.style[y.width > y.height ? "height" : "width"] = "100%"; y.style.alignSelf = "center"; x.appendChild(y) }, { once: true })
   y.addEventListener("click", () => open(location.origin + "/src/jpg/" + img + ".jpg", "_blank"))
   return x
 }
