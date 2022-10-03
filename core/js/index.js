0c7807eb99701d749eea68cc9acbeeb9166165d2
Aset Telmanov <telmanov2002.at@gmail.com>
2022-10-03 21:43:01 +0500

    Maybe ok

--- core/js/index.js
+++ core/js/index.js
@@ -5,105 +5,54 @@ const g = globalThis
 const d = document
 const body = d.body
 
-const headerHTML = `
-<div flex="row" header-container>
-  <button header-button>
-    <div header-icon></div>
-  </button>
-  <a href="/" button header-button header-hero>
-    <div header-icon>
-      <img src="https://titanium-studio.github.io/src/svg/hero1.svg" alt="hero">
-    </div>
-  </a>
-  <button onclick="headerOnClick()" header-button header-burger>
-    <div header-icon>
-      <span></span>
-      <span></span>
-    </div>
-  </button>
-</div>`
+const header = search.id("header")
+const content = search.id("body")
+const dialog = search.id("dialog")
 
-const navHTML = `
-<div>
-  <div></div>
-</div>`
+const pageLenght = 4
 
-/** @type {HTMLDivElement} */
-const view = search.id("view")
-/** @type {HTMLDivElement} */
-const dockStatus = search.id("status")
-const list = []
+let index = 0
+const sections = [
+  search.id("dsgn"),
+  search.id("work"),
+  search.id("skills"),
+  search.id("contacts")
+]
 
-function newCard({ name, value, img, href }, img_source_path, index) {
-  const card = search.new("a")
-  const photo = new Image()
-  const button = search.new("button")
-
-
-  if (is.str(img)) {
-    photo.src = img_source_path + img
+each(search.all(".name"),(name,i)=>{
+  name.onclick = ()=>{
+    openSection(i)
+    css.remove(body,"header_nav_active")
   }
 
-  if (is.str(href)) {
-    card.href = href
-  }
+})
 
-  card.classList.add("card","item")
-  button.onclick = () => { doScrollIndex(index) }
-  card.appendChild(photo)
-
-  return {
-    name,
-    value,
-    index,
-    card,
-    href,
-    button
+let lastScrollTime = 0
+const scrollAnimationDuration = 700
+content.addEventListener("wheel", (e) => {
+  e.preventDefault()
+  const currentTime = Date.now()
+  if (lastScrollTime + scrollAnimationDuration < currentTime) {
+    lastScrollTime = currentTime
+    onWheel(index + Math.sign(e.deltaY))
   }
+})
+
+function onWheel(newIndex) {
+  if (newIndex >= sections.length) {
+    newIndex = 0
+  }else if (newIndex < 0) {
+    newIndex = sections.length - 1
+  }
+  openSection(newIndex)
 }
 
-
-function doScrollIndex() { }
-
-
-fetch("/src/json/index.json")
-  .then((response) => (response.json()))
-  .then((json) => {
-    if (is.array(json.data) && is.str(json.img_source_path)) {
-      const { data, img_source_path } = json
-
-      each(data, (item, index) => {
-        const card = newCard(item, img_source_path)
-        view.appendChild(card.card)
-        dockStatus.appendChild(card.button)
-      })
-    }
-  })
-
-
-function onload() {
-  if (onload.once) { return }
-
-  onload.once++
-
-  // const content = search("#body")
-  const header = search.new("header")
-  const nav = search.new("div")
-
-  header.id = "header"
-  nav.id = "navbox"
-
-  header.innerHTML = headerHTML
-  nav.innerHTML = navHTML
-
-  g.headerOnClick = function () {
-    css.toggle(body, "header_nav_active")
-  }
-
-  body.prepend(header)
-  body.append(nav)
+g.headerOnClick = () => {
+  css.toggle(body, "header_nav_active")
 }
-onload.once = 0
 
-g.addEventListener("DOMContentLoaded", onload)
-g.addEventListener("load", onload)
+function openSection(targetIndex) {
+  if (0 <= targetIndex && targetIndex <= sections.length) {
+    scrollTo(sections[index = targetIndex])
+  }
+}
