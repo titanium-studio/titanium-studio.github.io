f8ee5dfa1eb9b3a4af1921eedb94ca21d92c6eab
x-titan <50516342+x-titan@users.noreply.github.com>
2022-11-14 20:36:13 +0500

    ok

--- index.js
+++ index.js
@@ -5,58 +5,47 @@ const g = globalThis
 const d = document
 const body = d.body
 
-const header = search.id("header")
-const content = search.id("body")
-const dialog = search.id("dialog")
+new Promise((res, rej) => {
+  const minLoadT = 200
+  const updT = 100
+  const maxLoadT = 7000
+  const startTime = Date.now()
 
-const pageLenght = 4
+  const update = () => {
+    const currT = Date.now()
+    if (
+      d.readyState === "complete"
+      && (startTime + minLoadT) < currT
+    ) {
+      return res()
+    }
 
-let index = 0
-const sections = [
-  search.id("dsgn"),
-  search.id("work"),
-  search.id("skills"),
-  search.id("contacts")
-]
+    if ((startTime + maxLoadT) < currT) {
+      rej()
+    }
 
-each(search.all(".name"),(name,i)=>{
-  name.onclick = ()=>{
-    openSection(i)
-    css.remove(body,"header_nav_active")
+    setTimeout(update, updT)
   }
 
+  update()
 })
+  .then(() => {
+    body.removeAttribute("loading")
+  })
+  .then(() => {
+    const pages = new Set([
+      "studio",
+      "gellery",
+      "about",
+      "projects",
+      "contacts"
+    ])
 
-let lastScrollTime = 0
-const scrollAnimationDuration = 700
-content.addEventListener("wheel", (e) => {
-  e.preventDefault()
-  const currentTime = Date.now()
-  if (lastScrollTime + scrollAnimationDuration < currentTime) {
-    lastScrollTime = currentTime
-    onWheel(index + Math.sign(e.deltaY))
-  }
-})
+    const hash = location.hash || ""
 
-function onWheel(newIndex) {
-  if (newIndex >= sections.length) {
-    newIndex = 0
-  }else if (newIndex < 0) {
-    newIndex = sections.length - 1
-  }
-  openSection(newIndex)
-}
-
-g.headerOnClick = () => {
-  css.toggle(body, "header_nav_active")
-}
-
-g.onresize =(e)=>{
-  openSection(index)
-}
-
-function openSection(targetIndex) {
-  if (0 <= targetIndex && targetIndex <= sections.length) {
-    scrollTo(sections[index = targetIndex])
-  }
-}
+    if (pages.has(hash.replace("#", "").toLowerCase())) {
+      scrollTo(search(hash))
+    } else {
+      location.hash = "#studio"
+    }
+  })
